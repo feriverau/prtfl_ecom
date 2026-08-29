19363a97b9851dea032ce7c63c9ef8c8800e5e67
Niafe <ferivera.ur@duocuc.cl>
2026-08-29 06:58:14 -0400

    Init repo: -Base product model (+slugify method) -App+env+settings configuration -Index & detail views, paths and templates (+index tailwind styling)

---
+++ mysite/myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Product
+
+# Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    return render(request,'myapp/index.html',{'products':products})
+
+def detail(request,slug):
+    product = Product.objects.get(slug=slug)
+    return render(request,'myapp/detail.html',{'product':product})
