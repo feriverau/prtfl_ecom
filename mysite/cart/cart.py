183740ddd53aca367bc55d67b97cdf980274a866
Niafe <ferivera.ur@duocuc.cl>
2026-08-30 22:51:45 -0400

    Saving cart data into session: cart class, view logic, context processors

---
+++ mysite/cart/cart.py
@@ -0,0 +1,15 @@
+class Cart():
+    def __init__(self,request):
+        self.session =  request.session
+        cart = request.session.get('cart')
+        if 'cart' not in request.session:
+            cart = self.session['cart']={}
+        self.cart = cart
+
+    def add(self,product,product_qty):
+        product_id = product.id
+        if product_id in self.cart:
+            self.cart[product_id]['qty']= product_qty
+        else:
+            self.cart[product_id]={'price':str(product.price),'qty':product_qty} 
+        self.session.modified=True
--- mysite/cart/views.py
+++ mysite/cart/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .cart import Cart
+from myapp.models import Product
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
 def cart_add(request):
+    cart = Cart(request)
     print("Botón del carro clickeado")
     if request.method=="POST":
         product_id = request.POST.get("product_id")
         product_quantity = request.POST.get("product_quantity")
         print("El producto agregado tiene la id:",product_id)
         print("La cantidad del producto es:",product_quantity)
+        #product = Product.objects.get(id=product_id)
+        product = get_object_or_404(Product,id=product_id)
+        cart.add(product=product,product_qty=product_quantity)
     return JsonResponse({'Message':'Botón del carro clickeado'})
