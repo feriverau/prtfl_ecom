eccfafcb5e6cfff53ec50f64a33113c6c75ec6fb
feriverau <167826552+feriverau@users.noreply.github.com>
2026-09-03 06:30:54 -0400

    Users app: register view, form & template(+tailwind styling)

---
+++ mysite/users/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .forms import CreateUserForm
+
+# Create your views here.
+def register(request):
+    form = CreateUserForm()
+    if request.method=="POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request,'users/register.html',{'form':form})
