e8fa1b2fbae236e83731101a754a58c022497450
Niafe <ferivera.ur@duocuc.cl>
2026-09-09 15:49:14 -0300

    Login&Logout functionality: LoginForm (extends AuthenticationForm); show conditional login/logout link in navbar (based on user.is_authenticated); views, paths, template(tailwind styling)

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -7,6 +7,8 @@ from django.utils.http import urlsafe_base64_decode,urlsafe_base64_encode
 from django.utils.encoding import force_bytes,force_str
 from .token import account_activation_token
 from django.contrib.auth.models import User
+from .forms import LoginForm
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
 def register(request):
@@ -49,3 +51,21 @@ def email_verification_success(request):
 
 def email_verification_failed(request):
     return render(request,'users/email-verification-failed.html')
+
+def user_login(request):
+    form = LoginForm()
+    if request.method=="POST":
+        form = LoginForm(request,data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('index')
+
+    return render(request,'users/login.html',{'form':form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('index')
