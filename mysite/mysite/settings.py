f77506adea37b324c788991f30c91f1829d3a7d2
Niafe <ferivera.ur@duocuc.cl>
2026-09-12 20:29:06 -0300

    Orders app: address model, form, view & template

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'myapp',
     'cart',
     'users',
+    'orders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ mysite/orders/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Address(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    line1 = models.CharField(max_length=255)
+    line2 = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+    province = models.CharField(max_length=100)
