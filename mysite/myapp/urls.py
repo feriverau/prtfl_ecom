c27f34fb98de289000bba21602d28e59490380a5
Niafe <ferivera.ur@duocuc.cl>
2026-09-16 10:26:16 -0300

    Search functionality implemented: view logic (+pagination,strip,srch by desc,etc.), path (+reorder), template logic+styling

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -3,5 +3,6 @@ from .import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('search',views.search,name='search'),
     path('<slug:slug>',views.detail,name='detail'),
 ] 
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 
@@ -14,3 +15,28 @@ def index(request):
 def detail(request,slug):
     product = Product.objects.get(slug=slug)
     return render(request,'myapp/detail.html',{'product':product})
+
+def search(request):
+
+    item_name = request.GET.get("item_name","").strip()
+
+    if item_name:
+        products = Product.objects.filter(
+            Q(name__icontains=item_name) |
+            Q(description__icontains=item_name)
+        )
+    else:
+        products = Product.objects.none()
+
+    paginator = Paginator(products,4)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        "myapp/search.html",
+        {
+            "page_obj": page_obj,
+            "item_name": item_name,
+        },
+    )
