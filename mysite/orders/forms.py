ca5999aef4dbec6ec8fde7e5ec619015a10c213b
Niafe <ferivera.ur@duocuc.cl>
2026-09-23 17:55:26 -0300

    App general styling and implementations (checkout&cart); templates+forms

--- mysite/orders/forms.py
+++ mysite/orders/forms.py
@@ -15,3 +15,17 @@ class AddressForm(forms.ModelForm):
             "postal_code": "Código postal",
             "province": "Provincia",
         }
+
+    def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+            for field in self.fields:
+                self.fields[field].widget.attrs.update({
+                    "class": (
+                        "w-full rounded-md border border-gray-300 "
+                        "px-3 py-2 "
+                        "focus:outline-none "
+                        "focus:ring-2 focus:ring-orange-500 "
+                        "focus:border-orange-500"
+                    )
+                })
